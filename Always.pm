b229dee4fc44b4fa57ca58d64e87c9b744ff0a8c
Michal Špaček <tupinek@gmail.com>
2015-09-16 14:55:25 +0200

    Improve SEE ALSO section in doc.

--- Always.pm
+++ Always.pm
@@ -108,20 +108,13 @@ L<Error::Pure>.
 
 =head1 SEE ALSO
 
-L<Error::Pure>,
-L<Error::Pure::AllError>,
-L<Error::Pure::Die>,
-L<Error::Pure::Error>,
-L<Error::Pure::ErrorList>,
-L<Error::Pure::HTTP::AllError>,
-L<Error::Pure::HTTP::Error>,
-L<Error::Pure::HTTP::ErrorList>,
-L<Error::Pure::HTTP::Print>,
-L<Error::Pure::HTTP::JSON>,
-L<Error::Pure::JSON>,
-L<Error::Pure::Output::JSON>,
-L<Error::Pure::Output::Text>,
-L<Error::Pure::Print>.
+=over
+
+=item L<Task::Error::Pure>
+
+Install the Error::Pure modules.
+
+=back
 
 =head1 ACKNOWLEDGMENTS
 
