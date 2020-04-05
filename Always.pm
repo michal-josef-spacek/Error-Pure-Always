4c6a1041a3dccccdd5c1f3bf0bf806f7e9b38456
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-04-05 14:52:07 +0200

    Update author username

--- Always.pm
+++ Always.pm
@@ -117,13 +117,13 @@ L<https://github.com/michal-josef-spacek/Error-Pure-Always>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© 2012-2015 Michal Špaček
+© 2012-2015 Michal Josef Špaček
 
 BSD 2-Clause License
 
